f720e3a1ca66aa09ee28199b48b15a2d2841a067
novichkovv <novichkovv@bk.ru>
2016-04-19 01:29:59 +0700

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
 //unset($_COOKIE['seconds']);
 ?>
 <!DOCTYPE html>
-<!-- saved from url=(0046)"autoid_1">
+<!-- saved from url=(0046)"autoid_1">-->
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Facebook</title>
